9cc8a3038f2bbdfdeb2dbe226491ef7eef38cd54
Andreas Blaette <andreas.blaette@uni-due.de>
2021-03-08 10:27:24 +0100

    corenlp,xml_document-method

--- R/corenlp.R
+++ R/corenlp.R
@@ -56,7 +56,77 @@ setMethod("corenlp_annotate", "data.table", function(x, corenlp_dir = getOption(
 })
 
 #' @rdname corenlp_annotate
+#' @examples
+#' xml_files <- list.files(system.file(package = "bignlp", "extdata", "xml"))
 setMethod("corenlp_annotate", "character", function(x, corenlp_dir = getOption("bignlp.corenlp_dir"), properties, byline = NULL, output_format = "json", threads = 1L, progress = TRUE,  preclean = TRUE, verbose = TRUE){
   stop("not implemented")
 })
 
+
+#' @rdname corenlp_annotate
+#' @importFrom xml2 read_xml xml_find_all xml_text xml_set_text xml_add_child xml_text<- write_xml
+#' @examples
+#' xml_dir <- system.file(package = "bignlp", "extdata", "xml")
+#' xml_files <- list.files(xml_dir, full.names = TRUE)
+#' xml_doc <- xml2::read_xml(x = xml_files[[1]])
+#' 
+#' Pipe <- StanfordCoreNLP$new(
+#'   output_format = "conll",
+#'   properties = corenlp_get_properties_file(lang = "en", fast = TRUE)
+#' )
+#' 
+#' xml_doc2 <- corenlp_annotate(x = xml_doc, pipe = Pipe, sentences = TRUE)
+#' 
+#' # Write annotated document to disc
+#' y <- tempfile(fileext = ".xml")
+#' xml2::write_xml(x = xml_doc, file = y, options = NULL)
+setMethod("corenlp_annotate", "xml_document", function(x, xpath = "//p", pipe, cols = c("word", "lemma", "pos"), sentences = TRUE, verbose = TRUE){
+  text_nodes <- xml2::xml_find_all(x = x, xpath)
+  text <- lapply(text_nodes, xml_text)
+  annolist <- AnnotationList$new(text)
+  
+  pipe$annotate(annolist, verbose = verbose)
+  
+  conll_outputter <- rJava::.jnew("edu.stanford.nlp.pipeline.CoNLLOutputter")
+  lapply(
+    0L:(annolist$obj$size() - 1L),
+    function(i){
+      tab <- read.table(
+        text = conll_outputter$print(annolist$obj$get(i)),
+        blank.lines.skip = TRUE, header = FALSE, sep = "\t", quote = ""
+      )
+      colnames(tab) <- c("idx", "word", "lemma", "pos", "ner", "headidx", "deprel")
+      if (isFALSE(sentences)){
+        m <- as.matrix(tab[, cols])
+        txt <- paste(apply(m, 1, function(row) paste(row, collapse = "\t")), collapse = "\n")
+        xml_set_text(x = text_nodes[[i + 1L]], value = sprintf("\n%s\n", txt))
+      } else {
+        if (nrow(tab) > 1L){
+          f <- cut(
+            1L:nrow(tab),
+            unique(c(which(tab[, "idx"] == 1L), nrow(tab))),
+            include.lowest = TRUE, right = FALSE
+          )
+          sentences <- split(x = tab, f = f)
+          lapply(
+            sentences,
+            function(s){
+              txt <- paste(apply(as.matrix(s[, cols]), 1, function(row) paste(row, collapse = "\t")), collapse = "\n")
+              xml_add_child(.x = text_nodes[[i + 1L]], .value = "s", sprintf("\n%s\n", txt))
+            }
+          )
+          xml_text(text_nodes[[i + 1L]]) <- ""
+        } else {
+          xml_add_child(
+          .x = text_nodes[[i + 1L]],
+          .value = "s",
+          sprintf("\n%s\n", paste(unlist(tab[, cols]), collapse = "\t"))
+        )
+          xml_text(text_nodes[[i + 1L]]) <- ""
+        }
+      }
+      invisible(NULL)
+    }
+  )
+  x
+})
